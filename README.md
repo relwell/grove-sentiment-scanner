ee5e1a7ddc16e2ed99860dad367043f62f1bee56
Robert Elwell <robert.elwell@gmail.com>
2017-02-10 20:46:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Grove Sentiment Scanner
+
+## Ingredients
+* Raspberry Pi 3
+* GrovePi+ Starter Kit
+* A Twitter API Key
+
+
+## Dependencies
+Since Grove dependencies can't be installed from pip, 
+why bother with a `requirements.txt`?
+
+```bash
+pip install nltk
+pip install twitter
+```
---
+++ scan.py
@@ -0,0 +1,29 @@
+from twitter import *
+from grove_rgb_lcd import *
+import os
+
+
+APP_NAME = os.env.get('My App')
+CONSUMER_KEY = os.env.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.env.get('CONSUMER_SECRET')
+
+MY_TWITTER_CREDS = os.path.expanduser('~/.my_app_credentials')
+
+def authenticated_stream():
+    """
+    Retrieves the Twitter stream. Will make you do stuff.
+    """
+    if not os.path.exists(MY_TWITTER_CREDS):
+        oauth_dance(APP_NAME, CONSUMER_KEY, CONSUMER_SECRET, MY_TWITTER_CREDS)
+        oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
+ 
+        return TwitterStream(auth=OAuth(oauth_token, oauth_token_secret, 
+                                        CONSUMER_KEY, CONSUMER_SECRET),
+                             domain="stream.twitter.com")
+
+
+
+
+if __name__ == "__main__":
+    for message in authenticated_stream().user():
+        print message
