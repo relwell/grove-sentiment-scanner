99b4468d8f339c29e97fc925fc201d5992c9b4a0
Robert Elwell <robert.elwell@gmail.com>
2017-02-10 21:03:35 -0500

    banner method

--- scan.py
+++ scan.py
@@ -1,13 +1,19 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from twitter import *
 from grove_rgb_lcd import *
 import os
 
+nltk.download('vader_lexicon')
 
 APP_NAME = os.getenv('APP_NAME', 'My App')
 CONSUMER_KEY = os.getenv('CONSUMER_KEY')
 CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
 
 MY_TWITTER_CREDS = os.path.expanduser('~/.my_app_credentials')
+
+ANALYZER = SentimentIntensityAnalyzer()
+
 
 def authenticated_stream():
     """
@@ -21,8 +27,17 @@ def authenticated_stream():
                                     CONSUMER_KEY, CONSUMER_SECRET))
 
 
+def banner(user, text, sentiment):
+    """
+    Display text in a banner, colored with the sentiment
+    """
+    setRGB(int(sentiment['neg'] * 255), int(sentiment['pos'] * 255), int(sentiment['neu'] * 255))
+    text = "%s %s " % (user, text)
+    text_len = len(text)
+    width = min([32, text_len])
+    [setText(text[i:i+width]) or time.sleep(0.1) for i in range(0, text_len)]
 
 
 if __name__ == "__main__":
     for message in authenticated_stream().statuses.sample():
-        print message
+        banner(message['user'], message['text'], ANALYZER.polarity_scores(message['text']))
